180ba453f7fcaae57764dccdda27d5ec1984eb2f
martin-bass <martin_reym@hotmail.com>
2023-01-30 10:19:39 -0300

    cambio App

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,21 @@ import { ChakraProvider, Container } from "@chakra-ui/react";
 
 //Components
 //import NavBar from '../src/components/NavBar';
-import Gallery from '../src/components/Gallery'
-import SimpleSidebar from '../src/components/NavBar'
-
-
+import Gallery from "../src/components/Gallery";
+import SimpleSidebar from "../src/components/NavBar";
 
 function App() {
   return (
     <ChakraProvider>
-      <SimpleSidebar children={undefined}/>
+      <SimpleSidebar />
       <Container
         minH={"100vh"}
         bgColor={"blackAlpha.200"}
         centerContent
         paddingX={0}
-        w={'1440px'}
-        overflowX='hidden'
-        maxW={'1440px'} 
+        w={"1440px"}
+        overflowX="hidden"
+        maxW={"1440px"}
       >
         {/* <NavBar /> */}
 

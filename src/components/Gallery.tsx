79d4f0a6d42f06203451261dc25114a0925de0ba
martin-bass <martin_reym@hotmail.com>
2023-01-31 13:42:08 -0300

    agregado detalle de foto

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,28 +1,46 @@
-import { Stack, Text, Image } from "@chakra-ui/react";
+import {
+  Stack,
+  Text,
+  Image,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+} from "@chakra-ui/react";
 
 //Masonry
 import Masonry from "react-masonry-css";
-import '../styles/Masonry.css';
+import "../styles/Masonry.css";
 
-import { Proyectos } from "../mock/types";
-
+import { Proyectos, Fotos } from "../mock/types";
+import { useState } from "react";
 
 interface Props {
-  projectFound : Proyectos
-};
+  projectFound: Proyectos;
+}
 
-export type projectFound = Proyectos
+function Gallery({ projectFound }: Props) {
+  const fotos: Fotos[] | any = projectFound.fotos;
 
-function Gallery({projectFound} : Props) {
-  
-  const fotos = projectFound.fotos;
-  console.log(projectFound.fotos);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const breakpointColumnsObj = {
     default: 5,
     1100: 4,
     700: 2,
-    500: 1
+    500: 1,
+  };
+
+  const [imgSelect, setImgSelect] = useState();
+
+  const handleSelect = (id: number) => {
+    onOpen();
+
+    const select = fotos.find((p: any) => p.id === id);
+    setImgSelect(select?.url);
   };
 
   return (
@@ -34,10 +52,39 @@ function Gallery({projectFound} : Props) {
           className="my-masonry-grid"
           columnClassName="my-masonry-grid_column"
         >
-          {fotos?.map((f) => (
-            <Image key={f.id} src={f.url} boxSize={250} h={"auto"} margin={4}/>
+          {fotos?.map((f: any) => (
+            <Image
+              key={f.id}
+              src={f.url}
+              boxSize={250}
+              h={"auto"}
+              margin={4}
+              onClick={() => handleSelect(f.id)}
+              cursor="pointer"
+            />
           ))}
         </Masonry>
+        {/* Modal */}
+        <Modal onClose={onClose} isOpen={isOpen} isCentered>
+          <ModalOverlay />
+          <ModalContent 
+          p={4} 
+          top={'30px'}
+          //w={'auto'}
+          >
+            <ModalCloseButton />
+            <ModalBody >
+              <Image 
+              src={imgSelect} 
+              //maxH={"700px"} 
+              //w={"auto"} 
+              h={'auto'}
+              //minW={'500px'}
+              />
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+        {/* Fin Modal */}
       </Stack>
     </Stack>
   );
--- src/components/ProjectDetail.tsx
+++ src/components/ProjectDetail.tsx
@@ -1,4 +1,3 @@
-
 import { Stack, VStack, Text } from "@chakra-ui/react";
 
 //mock
@@ -10,27 +9,26 @@ import Gallery from "../components/Gallery";
 import { ReactElement } from "react";
 
 export interface Props {
-  params: { url?: string }
-};
-
-
-    
+  params: { url?: string };
+}
 
 function ProjectDetail({ params }: Props) {
-    const projectFound: Proyectos | any = ArrayDeProyectos.Projects.find((proj) => {
-        if (proj.url === params.url) {
-          return proj;
-        }
-      });
+  const projectFound: Proyectos | any = ArrayDeProyectos.Projects.find(
+    (proj) => {
+      if (proj.url === params.url) {
+        return proj;
+      }
+    }
+  );
 
-  console.log(projectFound?.fotos);
   return (
     <Stack>
       <VStack pt={20} alignItems={"start"}>
         <Text>Titulo de la Obra: {projectFound?.title}</Text>
         <Text>Fecha de la obra: {projectFound?.fecha}</Text>
+        <Text>Descripcion: {projectFound?.description}</Text>
       </VStack>
-        <Gallery projectFound={projectFound}/>
+      <Gallery projectFound={projectFound} />
     </Stack>
   );
 }
